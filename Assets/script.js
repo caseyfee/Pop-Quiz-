e56b7eb5197945816ae4f77faa99df984543f311
caseyfee <caseyfee3@gmail.com>
2023-01-19 17:04:44 -0800

    rough framing of quiz

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,32 @@
-// Selects element by class
-var timeEl = document.querySelector(".time");
+// When you click the start button, the timer starts and you are presented with a question
+      // Add an event listener for the start buttom [In HTML] onclick that starts questions AND TIMER
+        // Likely a query selector - on click
+      // Create timer function that starts when button is pressed
+      // Question should pop up
+      
 
-// Selects element by id
-var mainEl = document.getElementById("main");
+// When you answer the question, you are presented with another question
+    // 
 
-var secondsLeft = 10;
+// When you answer incorrectly, time is subtracted from the clock
 
-function startTimer() {
+// When all questions are answered or the timer reaches 0, game is over
+
+
+// Whent the game is over, initials and score is saved
+
+
+
+
+// // Selects element by class
+// var timeEl = document.querySelector(".time");
+
+// // Selects element by id
+// var mainEl = document.getElementById("main");
+
+// var secondsLeft = 5;
+
+startButton.addEventListener("click", startTimer() {
   // Sets interval in variable
   var timerInterval = setInterval(function() {
     secondsLeft--;
@@ -18,35 +38,36 @@ function startTimer() {
       // Calls function to create and append image
       sendMessage();
     }
+  }
+    )}, 1000);
 
-  }, 1000);
 
-  function generateQuiz (questions, quizContainer, resultsContainer, submitButton) {
+  // function generateQuiz (questions, quizContainer, resultsContainer, submitButton) {
     
 
-    function rapidQuestions (questions, quizContainer) {
+  //   function rapidQuestions (questions, quizContainer) {
 
 
-    }
+  //   }
 
-    function answers (questions, quizContainer, resultsContainer) {
+  //   function answers (questions, quizContainer, resultsContainer) {
 
       
-    }
+  //   }
 
-  }
-}
+  // }
+
 
 
 // Function to end game
-function sendMessage() {
-  timeEl.textContent = " ";
-  var imgEl = document.createElement("img");
-  imgEl.setAttribute("src", "images/gameOver.jpeg");
-  mainEl.appendChild(imgEl);
+  function sendMessage() {
+     timeEl.textContent = " ";
+    var imgEl = document.createElement("img");
+    imgEl.setAttribute("src", "images/gameOver.jpeg");
+     mainEl.appendChild(imgEl);
 
-}
-function begin() {
-  document.getElementById("count");
-  startTimer();
-}
+// // }
+// function begin() {
+//   document.getElementById("count");
+//   startTimer();
+// }
